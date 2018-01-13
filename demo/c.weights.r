a7b2454df3d11d250cb9bd44968a2330302e3758
Andrey A Shabalin <andrey.shabalin@gmail.com>
2018-01-13 16:07:04 +0000

    version 2.2

--- demo/c.weights.r
+++ demo/c.weights.r
@@ -6,15 +6,15 @@ n = 100;
 # Number of covariates
 nc = 10;
 
-
-
+# Generate the standard deviation of the noise
+noise.std = 0.1 + rnorm(n)^2;
 
 # Generate the covariates
 cvrt.mat = 2 + matrix(rnorm(n*nc), ncol = nc);
 
 # Generate the vectors with genotype and expression variables
 snps.mat = cvrt.mat %*% rnorm(nc) + rnorm(n);
-gene.mat = cvrt.mat %*% rnorm(nc) + rnorm(n) + 0.5 * snps.mat + 1;
+gene.mat = cvrt.mat %*% rnorm(nc) + rnorm(n) * noise.std + 0.5 * snps.mat + 1;
 
 # Create 3 SlicedData objects for the analysis
 snps1 = SlicedData$new( matrix( snps.mat, nrow = 1 ) );
@@ -26,15 +26,15 @@ filename = NULL; # tempfile()
 
 # Call the main analysis function
 me = Matrix_eQTL_main(
-	snps = snps1, 
-	gene = gene1, 
-	cvrt = cvrt1, 
-	output_file_name = filename, 
-	pvOutputThreshold = 1, 
-	useModel = modelLINEAR, 
-	errorCovariance = numeric(), 
-	verbose = TRUE,
-	pvalue.hist = FALSE );
+    snps = snps1, 
+    gene = gene1, 
+    cvrt = cvrt1, 
+    output_file_name = filename, 
+    pvOutputThreshold = 1, 
+    useModel = modelLINEAR, 
+    errorCovariance = diag(noise.std^2), 
+    verbose = TRUE,
+    pvalue.hist = FALSE );
 
 # Pull Matrix eQTL results - t-statistic and p-value
 beta = me$all$eqtls$beta;
@@ -43,14 +43,14 @@ pvalue = me$all$eqtls$pvalue;
 rez = c(beta = beta, tstat = tstat, pvalue = pvalue);
 # And compare to those from the linear regression in R
 {
-	cat("\n\n Matrix eQTL: \n"); 
-	print(rez);
-	cat("\n R summary(lm()) output: \n");
-	lmdl = lm( gene.mat ~ snps.mat + cvrt.mat );
-	
-	lmout = summary(lmdl)$coefficients[2,c("Estimate","t value","Pr(>|t|)")];
-	print( lmout );
+    cat("\n\n Matrix eQTL: \n");
+    print(rez);
+    cat("\n R summary(lm()) output: \n");
+    lmdl = lm( gene.mat ~ snps.mat + cvrt.mat,
+                weights = 1/noise.std^2 );
+    lmout = summary(lmdl)$coefficients[2, c("Estimate", "t value", "Pr(>|t|)")];
+    print( lmout );
 }
 
 # Results from Matrix eQTL and "lm" must agree
-stopifnot(all.equal(lmout, rez, check.attributes=FALSE));
+stopifnot(all.equal(lmout, rez, check.attributes = FALSE));
